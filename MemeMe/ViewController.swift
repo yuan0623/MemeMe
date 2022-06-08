858a353cad9e6cee4c7b4dd1a873d430197c8f74
Yuan <gao308680162@gmail.com>
2022-06-07 22:50:36 -0400

    able to pick image, the activity view is coorrect

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -7,13 +7,128 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UITextFieldDelegate {
 
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var imagePickView: UIImageView!
+    @IBOutlet weak var TopTextField: UITextField!
+    @IBOutlet weak var BottomTextField: UITextField!
+    @IBOutlet weak var activity: UIButton!
+    @IBOutlet weak var cancel: UIButton!
+    
+    struct Meme{
+        let topText:String?
+        let bottomText:String?
+        let originalImage: UIImage?
+        let memedImage: UIImage?
+    }
+    
+    let memeTextAttributes: [NSAttributedString.Key:Any] = [
+        NSAttributedString.Key.strokeColor:UIColor.black,
+        NSAttributedString.Key.foregroundColor:UIColor.black,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth: 2
+        
+    ]
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        self.TopTextField.delegate = self
+        self.BottomTextField.delegate = self
+        TopTextField.defaultTextAttributes = memeTextAttributes
+        BottomTextField.defaultTextAttributes = memeTextAttributes
+        activity.isEnabled = false
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage{
+            imagePickView.image = image
+        }
+        
+        print("image is selected")
+        dismiss(animated: true)
+        activity.isEnabled = true
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
+    @IBAction func PickAnImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController,animated: true,completion: nil)
+    }
+    @IBAction func PIckAnImageFromCameral(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker,animated: true,completion: nil)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        
+        return true
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func subscribeToKeyboardNotifications() {
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
     }
 
+    func unsubscribeFromKeyboardNotifications() {
 
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+
+        view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    func generateMemedImage() -> UIImage {
+
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+
+        return memedImage
+    }
+    
+    
+
+    
+    @IBAction func UIActivity(_ sender: Any) {
+        let meme = Meme(topText: TopTextField.text, bottomText: BottomTextField.text, originalImage: imagePickView.image, memedImage: generateMemedImage())
+        let image = meme.memedImage
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        present(controller,animated: true,completion: nil)
+    }
+    
+    
 }
 
